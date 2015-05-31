dbdb87fb5d5968606a15e603c00a94418881d00a
jgcaap <jgcaap@gmail.com>
2015-05-30 22:24:40 -0400

    new neon
    Change-Id: Ica05f2769c40b78f1bf5979b7b85c68c28af3f80

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,7 +7,8 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifneq (,$(filter cortex-a15 denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15
+	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad
+        arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
 	arch_variant_cflags := -mcpu=cortex-a9

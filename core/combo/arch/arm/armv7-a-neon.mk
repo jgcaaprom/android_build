2d80025a36f4666a47699b58d6b5b721d28e76e1
jgcaap <jgcaap@gmail.com>
2015-06-03 17:46:35 -0400

    Correct optimizations for krait
    Change-Id: I98a77b85ccb5deac6495c000f18383cf40cf6166

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -6,9 +6,12 @@ ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
-ifneq (,$(filter cortex-a15 denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
-        arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
+ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+        arch_variant_cflags := -mfpu=neon-vfpv4 \
+    -march=armv7-a \
+    -D__ARM_FEATURE_LPAE=1 \
+    -D__ARM_FEATURE_VFP4=1 \
+    --param l1-cache-size=16 --param l1-cache-line-size=16 --param l2-cache-size=2048
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
 	arch_variant_cflags := -mcpu=cortex-a9
@@ -19,6 +22,9 @@ else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7
 else
+ifneq (,$(filter cortex-a15 denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+        arch_variant_cflags := -mcpu=cortex-a15
+else
 	arch_variant_cflags := -march=armv7-a
 endif
 endif
@@ -27,7 +33,3 @@ endif
 
 arch_variant_cflags += \
     -mfloat-abi=softfp \
-    -mfpu=neon
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8

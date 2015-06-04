d44e06cc1c52e9c6d739601d518511ade995f1fd
jgcaap <jgcaap@gmail.com>
2015-06-04 11:07:50 -0400

    removing aligment until ther is more informatio about the ideal size
    Change-Id: I94a29380a2baf8a5d8ad30e2934c8ee75b00b68d

--- core/jgcaap.mk
+++ core/jgcaap.mk
@@ -8,8 +8,9 @@ NEW_GCC_CFLAGS_THUMB := -O2 -DNDEBUG
 # Additional flags passed to all C targets compiled with GCC
 NEW_GCC_CFLAGS := $(ALIGNED) -DNDEBUG
 
-ALIGNED = -O2 -floop-flatten -floop-parallelize-all -ftree-loop-linear -floop-interchange -floop-strip-mine -floop-block -fgcse-las -ftree-slp-vectorize -ffunction-sections  -funwind-tables -fstack-protector -ftree-vectorize -fpredictive-commoning -fgcse-las -fgcse-lm -fgcse-sm -fsched-spec-load -Wno-invalid-command-line-argument -Wno-unused-command-line-argument -falign-functions=16 -falign-loops=16 
+ALIGNED = -O2 -floop-flatten -floop-parallelize-all -ftree-loop-linear -floop-interchange -floop-strip-mine -floop-block -fgcse-las -ftree-slp-vectorize -ffunction-sections  -funwind-tables -fstack-protector -ftree-vectorize -fpredictive-commoning -fgcse-las -fgcse-lm -fgcse-sm -fsched-spec-load -Wno-invalid-command-line-argument -Wno-unused-command-line-argument
 
+#-falign-functions=16 -falign-loops=16 
 #--param l1-cache-size=16 --param l1-cache-line-size=16 --param l2-cache-size=2048
 
 # Flags passed to all C targets compiled with GCC

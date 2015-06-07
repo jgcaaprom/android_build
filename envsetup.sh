4b3a47471919d559a88cb85d981fe673c8afd5a7
jgcaap <jgcaap@gmail.com>
2015-06-07 11:28:42 -0400

    shamu
    Change-Id: I4de563c2f147921d73e8d1d62e1b5f5a5db95dc2

--- envsetup.sh
+++ envsetup.sh
@@ -178,7 +178,7 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-eabi-5.1/bin
+            toolchaindir=arm/arm-eabi-4.8/bin
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":

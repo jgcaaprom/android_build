33e046956e39f85f91decc166f5f82dc6c336c46
Jorge Pereira <jgcaap@gmail.com>
2016-02-08 05:39:50 -0600

    Ensure we use this art set

--- core/jgcaap.mk
+++ core/jgcaap.mk
@@ -1,3 +1,7 @@
 DEBUG_SYMBOL_FLAGS := -g0 -DNDEBUG
 DEBUG_FRAME_POINTER_FLAGS := -fomit-frame-pointer
-#USE_PROGUARD_OPTIMIZATIONS := true
+USE_PROGUARD_OPTIMIZATIONS := true
+
+PRODUCT_PROPERTY_OVERRIDES += \ 
+     dalvik.vm.dex2oat-filter=interpret-only \
+     dalvik.vm.image-dex2oat-filter=speed

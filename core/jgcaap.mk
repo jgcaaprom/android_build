7dba49349edfb1bd1c40a6358599f797d76ece49
Jorge Pereira <jgcaap@gmail.com>
2016-02-14 21:50:55 -0600

    Revert "set speed"
    This reverts commit 5855b874ee0bfe7db2ca2c892744562483b17426.

--- core/jgcaap.mk
+++ core/jgcaap.mk
@@ -3,7 +3,11 @@ DEBUG_FRAME_POINTER_FLAGS := -fomit-frame-pointer
 USE_PROGUARD_OPTIMIZATIONS := true
 
 PRODUCT_PROPERTY_OVERRIDES += \ 
-     dalvik.vm.dex2oat-filter=speed \
+     dalvik.vm.dex2oat-filter=interpret-only \
      dalvik.vm.image-dex2oat-filter=speed
+
+ art_cflags += \
+    $(DEBUG_SYMBOL_FLAGS) \
+    $(DEBUG_FRAME_POINTER_FLAGS)
 
 BOARD_USE_LZ4_RD_COMPRESSOR := true
